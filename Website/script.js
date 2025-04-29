c241727c5e9744d4d9ec20948c32ee5cbe555e7c
Nur ASLAN <148655598+Nurasln@users.noreply.github.com>
2025-04-29 15:50:24 +0300

    SoggyByte

---
+++ Website/script.js
@@ -0,0 +1,54 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link")
+const menuOpenButton = document.querySelector("#menu-open-button")
+const menuCloesButton = document.querySelector("#menu-close-button")
+
+menuOpenButton.addEventListener("click", () => {
+    //Toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu")
+})
+
+menuCloesButton.addEventListener("click", () => menuOpenButton.click())
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click());
+});
+
+//Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    //Responsive Breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        1024: {
+            slidesPerView: 2
+        },
+    }
+
+});
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
