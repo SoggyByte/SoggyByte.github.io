37544af60c018e8fbb0998be0c947600187f2387
Nur ASLAN <148655598+Nurasln@users.noreply.github.com>
2025-05-02 21:33:01 +0300

    Game added

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const menuCloseButton = document.querySelector("#menu-close-button")
 menuOpenButton.addEventListener("click", () => {
     // Toggle mobile menu visibility
     document.body.classList.toggle("show-mobile-menu")
+    document.body.classList.toggle("no-scroll"); // Scroll engelleme
+    swiper.enabled = !swiper.enabled;
 })
 
 menuCloseButton.addEventListener("click", () => menuOpenButton.click())
